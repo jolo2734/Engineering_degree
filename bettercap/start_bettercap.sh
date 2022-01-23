c704f251f18d920c532014ee2e1574a8104fb39b
jolo2734 <bartek.matejuk.szkola@gmail.com>
2022-01-23 23:24:32 +0000

    crontab fix

--- bettercap/start_bettercap.sh
+++ bettercap/start_bettercap.sh
@@ -2,9 +2,7 @@
 
 #filtrowanie wszystkich przechwytywania
 sudo rm /home/ubuntu/credentials-bettercap/*
-grep -in -E "pass|login|password|user" $HOME/bettercap-logs/* > $HOME/credentials-bettercap/credentials_$(date +%d-%m-%Y_%I:%M%p).txt
+grep -in -E "pass|login|password|user" /home/ubuntu/bettercap-logs/* > /home/ubuntu/credentials-bettercap/credentials_$(date +%d-%m-%Y_%I:%M%p).txt
 
-sudo ./$HOME/inzynierka/bettercap/start_bettercap.sh
+sudo /home/ubuntu/inzynierka/bettercap/start_bettercap.sh
 
-touch /home/ubuntu/plik_testowy1.txt
-touch $HOME/plik_testowy2.txt
