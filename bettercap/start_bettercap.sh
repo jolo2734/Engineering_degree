17490422740ea442d700e31650fd270fc2a21c95
jolo2734 <bartek.matejuk.szkola@gmail.com>
2022-01-23 22:43:33 +0000

    new bettercap run default at start

--- bettercap/start_bettercap.sh
+++ bettercap/start_bettercap.sh
@@ -4,3 +4,4 @@
 sudo rm /home/ubuntu/credentials-bettercap/*
 grep -in -E "pass|login|password|user" $HOME/bettercap-logs/* > $HOME/credentials-bettercap/credentials_$(date +%d-%m-%Y_%I:%M%p).txt
 
+sudo ./bettercap
