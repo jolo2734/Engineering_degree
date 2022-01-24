6628755bb252abd5fcc3a7dded198f0983c91537
jolo2734 <bartek.matejuk.szkola@gmail.com>
2022-01-24 15:24:55 +0000

    add caplets, repair connection priority

--- automation/connect.sh
+++ automation/connect.sh
@@ -7,13 +7,13 @@ echo $WLAN
 echo 'network={' > /etc/wpa_supplicant.conf
 echo '        ssid="NETIASPOT-CDD280"' >> /etc/wpa_supplicant.conf
 echo '        psk=fe892596e52b49d5511477bf009577cce945be7477ec71d289de6beab174fb18' >> /etc/wpa_supplicant.conf
-echo '        priority=1' >> /etc/wpa_supplicant.conf
+echo '        priority=2' >> /etc/wpa_supplicant.conf
 echo '        id_str="HOME"' >> /etc/wpa_supplicant.conf
 echo '}' >> /etc/wpa_supplicant.conf
 echo 'network={' >> /etc/wpa_supplicant.conf
 echo '        ssid='$WLAN >> /etc/wpa_supplicant.conf
 echo '        key_mgmt=NONE' >> /etc/wpa_supplicant.conf
-echo '        priority=2' >> /etc/wpa_supplicant.conf
+echo '        priority=1' >> /etc/wpa_supplicant.conf
 echo '        id_str="STUDY"' >> /etc/wpa_supplicant.conf
 echo '}' >> /etc/wpa_supplicant.conf
 
