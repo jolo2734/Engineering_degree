c4df42cbcc4c101c3dff16c291bdfe587bf5bd26
jolo2734 <bartek.matejuk.szkola@gmail.com>
2022-02-01 03:33:33 +0000

    bettercap conffig, cleaning

--- bettercap/start_bettercap.sh
+++ bettercap/start_bettercap.sh
@@ -4,5 +4,5 @@
 sudo rm /home/ubuntu/credentials-bettercap/*
 grep -in -E "pass|login|password|user" /home/ubuntu/bettercap-logs/* > /home/ubuntu/credentials-bettercap/credentials_$(date +%d-%m-%Y_%I:%M%p).txt
 
-sudo /home/ubuntu/bettercap-2.32.0/bettercap -caplet /home/ubuntu/inzynierka/bettercap/caplets/http_passwords.cap  > /home/ubuntu/bettercap-logs/bettercap_$(date +%d-%m-%Y_%I:%M%p).txt
+sudo /home/ubuntu/bettercap-2.32.0/bettercap -iface wlan0 -caplet /home/ubuntu/inzynierka/bettercap/http_passwords.cap  > /home/ubuntu/bettercap-logs/bettercap_$(date +%d-%m-%Y_%I:%M%p).txt
 
