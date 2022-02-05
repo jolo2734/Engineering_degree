1795a546e5530fb70622a8d275c41b1ff2425bde
jolo2734 <bartek.matejuk.szkola@gmail.com>
2022-02-05 16:08:54 +0000

    all work,but without adapter

--- bettercap/start_bettercap.sh
+++ bettercap/start_bettercap.sh
@@ -4,5 +4,5 @@
 sudo cp -r /home/ubuntu/inzynierka/bettercap/hstshijack /usr/local/share/bettercap/caplets
 
 #uruchomienie programu bettercap
-sudo /home/ubuntu/bettercap-2.32.0/bettercap -iface eth0 -caplet /usr/local/share/bettercap/caplets/hstshijack/hstshijack.cap > /home/ubuntu/dane_inzynierka/bettercap-logs/bettercap_$(date +%d-%m-%Y_%I:%M%p).txt
+sudo /home/ubuntu/bettercap-2.32.0/bettercap -iface eth0 -caplet /usr/local/share/bettercap/caplets/hstshijack/hstshijack.cap | cat | tee /home/ubuntu/dane_inzynierka/bettercap-logs/bettercap_$(date +%d-%m-%Y_%I:%M%p).txt
 
